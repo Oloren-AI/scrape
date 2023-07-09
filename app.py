f0789719fd0bd4d512f8dd7e2e5305cfb6b45eda
Raunak Chowdhuri <raunak@oloren.ai>
2023-07-08 18:09:48 -0700

    added brightdata

--- app.py
+++ app.py
@@ -1,41 +1,33 @@
 import oloren as olo
-from serpapi import GoogleSearch
+import ssl
+import json
+import urllib.request
+import urllib.parse
+import os
 
 
-@olo.register(description="Use SERP API to export Google results to JSON")
-def scrape_google(api_key=olo.String(secret=True), query=olo.String()):
-    params = {
-        "q": query,
-        "location": "Austin, Texas, United States",
-        "hl": "en",
-        "gl": "us",
-        "google_domain": "google.com",
-        "api_key": api_key,
-    }
+@olo.register(
+    description="Use brightdata proxy to convert search queries to JSON. Requires BRIGHT_DATA_HOST, BRIGHT_DATA_USERNAME, BRIGHT_DATA_PASSWORD environment variables to be set."
+)
+def brightdata_serp(search=olo.String()):
+    engine = "https://www.google.com/search"  # can make this parameter once defaults are implemented
 
-    search = GoogleSearch(params)
-    results = search.get_dict()
-    organic_results = results["organic_results"]
-    return organic_results
+    ssl._create_default_https_context = ssl._create_unverified_context
 
+    BRIGHT_DATA_HOST = os.environ["BRIGHT_DATA_HOST"]
+    BRIGHT_DATA_USERNAME = os.environ["BRIGHT_DATA_USERNAME"]
+    BRIGHT_DATA_PASSWORD = os.environ["BRIGHT_DATA_PASSWORD"]
 
-@olo.register(description="Human readable SERP")
-def pretty_serp_convert(results=olo.Json()):
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    ret = []
-    for i, result in enumerate(results):
-        ret.append(f"{alphabet[i]}) {result['title']} ({result['source']}) - {result['snippet']}")
-    return "\n".join(ret)
-
-
-@olo.register()
-def parse_gpt(inp=olo.String()):
-    results = []
-    for line in inp.split("\n"):
-        if "=" in line:
-            score, description = line.split("=")[1].strip().split("|")
-            results.append({"score": int(score), "description": description})
-    return results
+    opener = urllib.request.build_opener(
+        urllib.request.ProxyHandler(
+            {
+                "http": f"http://{BRIGHT_DATA_USERNAME}:{BRIGHT_DATA_PASSWORD}@{BRIGHT_DATA_HOST}",
+                "https": f"http://{BRIGHT_DATA_USERNAME}:{BRIGHT_DATA_PASSWORD}@{BRIGHT_DATA_HOST}",
+            }
+        )
+    )
+    search = urllib.parse.quote(search)
+    return json.loads(opener.open(f"{engine}?q={search}&gl=us&lum_json=1").read())
 
 
 if __name__ == "__main__":
